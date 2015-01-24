348c4549cdbf84f09df72f276aa9af4e3da1cb15
DouglasPitangui <douglaspitangui@gmail.com>
2015-01-23 23:30:28 -0200

    Commit 1

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>INPROV</title>
+      <meta name="viewport" content="width=device-width">
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.js"></script>
+      <script src="js/inputmask-plugin.js"></script>
+   </head>
+   <body>
+      <nav class="navbar navbar-default navbar-xed-top navbar-cor">
+         <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">INPROV</a>
+            <button class="navbar-toggle" type="button"
+               data-target=".navbar-collapse" data-toggle="collapse">
+            <span class="glyphicon glyphicon-align-justify"></span></button>
+         </div>
+         <ul class="nav navbar-nav collapse navbar-collapse">
+            <li><a href="#">Colabore <span class="glyphicon glyphicon-book"></span></a></li>
+            <li><a href="#">Indique um amigo <span class="glyphicon glyphicon-user"></span></a></li>
+            <li><a href="#">Perguntas frequentes <span class="glyphicon glyphicon-list-alt"></span></a></li>
+            <li><a href="#">Sobre <span class="glyphicon glyphicon-bullhorn"></span></a></li>
+            <li><a href="#">Entre em contato <span class="glyphicon glyphicon-envelope"></span></a></li>
+         </ul>
+      </nav>
+	  
+      <div class="container">
+         <div class="row">
+            <fieldset class="col-md-6">
+               <label for="Curso">Curso:</label>
+               <select name="bandeira-cartao" id="bandeira-cartao" class="form-control" width:50%>
+                  <option value="master">Ciência da Computação</option>
+                  <option value="visa">VISA</option>
+                  <option value="amex">American Express</option>
+               </select>
+            </fieldset>
+            <fieldset class="col-md-6">
+               <label for="Curso">Disciplina:</label>
+               <select name="bandeira-cartao" id="bandeira-cartao" class="form-control" width:50%>
+                  <option value="master">Cálculo I</option>
+                  <option value="visa">VISA</option>
+                  <option value="amex">American Express</option>
+               </select>
+            </fieldset>
+         </div>
+		 ************************************************
+		 <div class="bs-example" data-example-id="table-within-panel">
+    <div class="panel panel-default">
+      <!-- Default panel contents -->
+      <div class="panel-heading">Panel heading</div>
+      <div class="panel-body">
+        <p>Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+
+      <!-- Table -->
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+		 *********************************************
+      </div>
+   </body>
+</html>
