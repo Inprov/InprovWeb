ca358343d7a1313a57be16166283a3721f219ba8
DouglasPitangui <douglaspitangui@gmail.com>
2015-01-25 13:38:15 -0200

    exibindo imagem

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
       <meta charset="UTF-8">
       <title>INPROV</title>
       <meta name="viewport" content="width=device-width">
-      <link rel="stylesheet" href="css/bootstrap.css">
+      <link rel="stylesheet" href="css/bootstrap-cosmo.css">
       <script src="js/jquery.js"></script>
       <script src="js/bootstrap.js"></script>
       <script src="js/inputmask-plugin.js"></script>
-	  <!-- APENAS PARA EXIBIÇÃO DE IMAGEM -->
- 	 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-	 <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-	<link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
-	  <!-- FIM DOS LINK PAR AXIBICAO DE IMAGEM -->
+      <!-- APENAS PARA EXIBIÇÃO DE IMAGEM -->
+      <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+      <link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
+      <!-- FIM DOS LINK PAR AXIBICAO DE IMAGEM -->
       <link rel="stylesheet" href="css/index.css">
    </head>
    <body>
@@ -36,7 +35,6 @@
             <li><a href="#">Entre em contato <span class="glyphicon glyphicon-envelope"></span></a></li>
          </ul>
       </nav>
-	  
       <div class="container">
          <!--***********************************************************************************
             *																																*
@@ -74,208 +72,195 @@
                <table class="table table-responsive">
                   <thead>
                      <tr>
-
-						 <th >Curso</th>
+                        <th >Curso</th>
                         <th>Disciplina</th>
-						  <th>Professor</th>
-						<th>Aplicada em</th>
-						<th>Visualizar</th>
-						<th>Download</th>
+                        <th>Professor</th>
+                        <th>Aplicada em</th>
+                        <th>Visualizar</th>
+                        <th>Download</th>
                      </tr>
                   </thead>
                   <tbody>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><a href="img/Algebra - Elvira Padua 05-05-2010/Algebra - Elvira Padua 05-05-2010 parte1.jpg" title="prova 1" data-gallery = "teste"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><a href="img/tess.png" title="prova 1" data-gallery="imagem"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>
-					 <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>                     <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>                     <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>                     <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>                     <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
-                     </tr>                     <tr>
-						<td>NOME DO CURSO</td>
-                        <td>NOME DA DISCIPLINA</td>
-                        <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
                      <tr>
-						<td>NOME DO CURSO</td>
+                        <td>NOME DO CURSO</td>
                         <td>NOME DA DISCIPLINA</td>
                         <td>NOME DO PROFESSOR</td>
-						<td>DATA DE APLICAÇÃO</td>
-						<td><span class="glyphicon glyphicon-eye-open"></span></td>
-						<td><span class="glyphicon glyphicon-download"></span></td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
                      </tr>
-					 
-
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
+                     <tr>
+                        <td>NOME DO CURSO</td>
+                        <td>NOME DA DISCIPLINA</td>
+                        <td>NOME DO PROFESSOR</td>
+                        <td>DATA DE APLICAÇÃO</td>
+                        <td><span class="glyphicon glyphicon-eye-open"></span></td>
+                        <td><span class="glyphicon glyphicon-download"></span></td>
+                     </tr>
                   </tbody>
                </table>
             </div>
          </div>
-		 
-<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
-<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" style="display: none;">
-    <!-- The container for the modal slides -->
-    <div class="slides"></div>
-    <!-- Controls for the borderless lightbox -->
-    <h3 class="title"></h3>
-    <a class="prev">‹</a>
-    <a class="next">›</a>
-    <a class="close">×</a>
-    <a class="play-pause"></a>
-    <ol class="indicator"></ol>
-    <!-- The modal dialog, which will be used to wrap the lightbox content -->
-    <div class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title"></h4>
-                </div>
-                <div class="modal-body next"></div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left prev">
+         <!-- Galeria de imagem -->
+         <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" style="display: none;">
+            <!-- slides -->
+            <div class="slides"></div>
+            <!-- controle para passar imagem lightbox -->
+            <h3 class="title"></h3>
+            <a class="prev">‹</a>
+            <a class="next">›</a>
+            <a class="close">×</a>
+            <a class="play-pause"></a>
+            <ol class="indicator"></ol>
+            <!-- visualizar imagem em form lightbox -->
+            <div class="modal fade">
+               <div class="modal-dialog">
+                  <div class="modal-content">
+                     <div class="modal-header">
+                        <button type="button" class="close" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title"></h4>
+                     </div>
+                     <div class="modal-body next"></div>
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-default pull-left prev">
                         <i class="glyphicon glyphicon-chevron-left"></i>
                         Previous
-                    </button>
-                    <button type="button" class="btn btn-primary next">
+                        </button>
+                        <button type="button" class="btn btn-primary next">
                         Next
                         <i class="glyphicon glyphicon-chevron-right"></i>
-                    </button>
-                </div>
+                        </button>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-</div>
+         </div>
 
-
-
-
-<div id="links">
-    <a href="img/1.jpg" title="prova 1" data-gallery>
-        <img src="img/1.jpg" alt="1">
-    </a>
-    <a href="img/2.png" title="prova 2" data-gallery>
-         <img src="img/2.png" alt="2">
-    </a>
-    <a href="img/3.png" title="prova 3" data-gallery>
-         <img src="img/3.png" alt="3">
-    </a>
-</div>
+		 <a href="img/Algebra - Elvira Padua 05-05-2010/Algebra - Elvira Padua 05-05-2010 parte2.jpg" title="prova 1" data-gallery = "teste"><span class="glyphicon glyphicon-eye-open"></span></a>
+		 <a href="img/Algebra Linear - Maria Alice - 28-02-2011/Algebra Linear - Maria Alice - 28-02-2011 parte1.jpg" title="prova 1" data-gallery = "teste2"><span class="glyphicon glyphicon-eye-open"></span></a>
 		 
       </div>
-	  
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
-
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
    </body>
 </html>
